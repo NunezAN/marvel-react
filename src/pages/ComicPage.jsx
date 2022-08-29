a0af29cb539b8aebd0ce170ead7065a929df0da1
NunezAN <alex101nunez@gmail.com>
2022-08-29 17:44:35 -0500

    comic page is styled, todo-add loading state css

--- src/pages/ComicPage.jsx
+++ src/pages/ComicPage.jsx
@@ -1,29 +1,59 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import "./ComicPage.css";
+import { Link, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const ComicPage = () => {
   const location = useLocation();
   const comicData = location.state;
   return (
-    <>
-      <h1>{comicData.title}</h1>
-      <img
-        className="comic__img"
-        src={`${comicData.thumbnail.path}/portrait_uncanny.${comicData.thumbnail.extension}`}
-        alt=""
-      />
-      <p className="comic__year">
-        DATE RELEASED: {comicData.dates[0].date.substring(0, 10)}
-      </p>
-      <p className="comic__description--para">
-        {comicData.description || "NO DESCRIPTION AVAILABLE"}
-      </p>
-      <p>
-        Series: {comicData.series.name}
-      </p>
-      <p>Page Count {comicData.pageCount}</p>
-      <p>Price: ${comicData.prices[0].price}</p>
-    </>
+    <div className="comicPage">
+      <Link to="/Search" className="comicPage__back--wrapper">
+        <FontAwesomeIcon className="back--icon" icon={faArrowLeft} />
+      </Link>
+      <div className="comicPage__container">
+        <div className="comicPage__img--wrapper">
+          <img
+            className="comicPage__img"
+            src={`${comicData.thumbnail.path}/portrait_uncanny.${comicData.thumbnail.extension}`}
+            alt=""
+          />
+        </div>
+        <div className="comicPage__details--wrapper">
+          <div className="comicPage__details__title--wrapper">
+            <h1 className="comicPage__title">{comicData.title}</h1>
+          </div>
+          <div className="comicPage__details">
+            <h1 className="comicPage__details__title">Comic Details</h1>
+            <p className="comic__details__para">
+              <span className="comicPage__details--subtitle">
+                DATE RELEASED:{" "}
+              </span>{" "}
+              {comicData.dates[0].date.substring(0, 10)}
+            </p>
+            <p className="comic__details__para">
+              <span className="comicPage__details--subtitle">
+                Description:{" "}
+              </span>
+              {comicData.description || "NO DESCRIPTION AVAILABLE"}
+            </p>
+            <p className="comic__details__para">
+              <span className="comicPage__details--subtitle">Series:</span>{" "}
+              {comicData.series.name}
+            </p>
+            <p className="comic__details__para">
+              <span className="comicPage__details--subtitle">Page Count:</span>{" "}
+              {comicData.pageCount}
+            </p>
+            <p className="comic__details__para">
+              <span className="comicPage__details--subtitle">Price:</span> $
+              {comicData.prices[0].price}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
