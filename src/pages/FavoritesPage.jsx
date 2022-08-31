381d46b40f579bd6eee45b2f402d7dccadf20276
NunezAN <alex101nunez@gmail.com>
2022-08-30 20:36:11 -0500

    favorites displayed in favorites page

--- src/pages/FavoritesPage.jsx
+++ src/pages/FavoritesPage.jsx
@@ -1,11 +1,65 @@
-import React from 'react';
+import { faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { collection, getDocs } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Comic from "../components/Comic";
+import { selectLoading, startLoading, stopLoading } from "../features/HeroSlice";
+import { db } from "../firebase";
+import "./FavoritesPage.css";
 
 const FavoritesPage = () => {
-    return (
-        <div>
-            Favorites
-        </div>
-    );
-}
+  const [favorites, setFavorites] = useState([]);
+  const isLoading = useSelector(selectLoading);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function getAllFavorites() {
+      dispatch(startLoading());
+      const favoritesData = await getDocs(collection(db, "favorites"));
+      const data = favoritesData.docs.map((comic) => ({
+        ...comic.data(),
+        id: comic.id,
+      }));
+      console.log(data);
+      setFavorites(data);
+      dispatch(stopLoading());
+    }
+
+    // onSnapshot(collection(db, "favorites"), (snapshot) => {
+    //   setFavorites(
+    //     snapshot.docs.map((elem) => ({ id: elem.id, data: elem.data() }))
+    //   );
+    //   //   console.log(favorites);
+    // });
+    getAllFavorites();
+  }, []);
+
+  return (
+    <div className="favoritesPage">
+      <div className="favoritesPage__title--container">
+        <h1 className="favoritePage__title">My Favorites</h1>
+      </div>
+      <div className="favoritesPage__comics--wrapper">
+      {isLoading ? (
+          <div className="loading__container">
+            <FontAwesomeIcon icon={faSpinner} className="loading--icon spin" />
+          </div>
+        ) : (
+            favorites.length > 0 && (
+            <div className="comic__results">
+              <div className="comic__results__comics--wrapper">
+                {favorites.length > 0 &&
+                  favorites.map((comic) => (
+                    <Comic key={comic.id} comicData={comic} />
+                  ))}
+              </div>
+            </div>
+          )
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default FavoritesPage;
